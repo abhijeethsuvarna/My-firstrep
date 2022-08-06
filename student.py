7dd23cc807b9c942b61af43eeb6dc198e5932059
abhijeethsuvarna <110712873+abhijeethsuvarna@users.noreply.github.com>
2022-08-06 13:06:20 +0530

    Add files via upload

---
+++ student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import pandas as pd
+
+
+# In[11]:
+
+
+pd.read_csv('student.tsv',sep='\t')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
